cb2c39f0ab0e25ade877007c130a91c5fd5b1d4e
Lilia de Tassia <lylyatavares@gmail.com>
2022-01-12 19:33:08 -0200

    Navigation buttons

--- frontend/src/componets/MovieCard/index.tsx
+++ frontend/src/componets/MovieCard/index.tsx
@@ -16,7 +16,11 @@ function MovieCard() {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+
             </div>
         </div>
     );
